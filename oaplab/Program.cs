5cc4146b79f3af31cb74f0c473d22ca6781ed3e1
RuslanSmir <78635318+RuslanSmir@users.noreply.github.com>
2021-06-10 14:25:24 +0300

    Update Program.cs

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 // namespace такой же как и название проекта
-namespace LabaSokolov
+namespace LabaSmirnov
 {
     // про классы мы пока не говорили...
     class Program
